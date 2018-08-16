14c6af2b073911e4f4ae209192a9e88dea9aa1f5
Gaspar Ricci <gasparricci.p95@gmail.com>
2018-08-16 17:09:58 -0300

    Satisface requisito 45. * Agrega views para cambiar campos de un espacio y se agregan los mapeos de URL necesarios para acceder a ellas. * Cuando un administrador accede a una ficha de espacio, el template muestra botones que abren modales para editar los campos. * Los usuarios (no admins) ven la ficha sin estos controles. Los divs modales no se generan en este caso.

--- cc4401Inventory/spacesApp/views.py
+++ cc4401Inventory/spacesApp/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from articlesApp.models import Article
 from spacesApp.models import Space
 from django.contrib import messages
+import os
 # Create your views here.
 
 @login_required
@@ -29,10 +30,53 @@ def space_data(request, space_id):
     try:
         space = Space.objects.get(id=space_id)
         context = {
-            'space': space
+            'space': space,
+            'user_is_staff': request.user.is_staff
         }
         return render(request, 'spacesApp/space_data.html', context)
     except Exception as ex:
         print('Exception in space_data.')
         print(ex)
         return redirect('/')
+
+@login_required
+def space_edit_name(request, space_id):
+
+    if request.user.is_staff and request.method == "POST":
+        s = Space.objects.get(id=space_id)
+        s.name = request.POST["name"]
+        s.save()
+    return redirect('/space/'+str(space_id))
+
+
+@login_required
+def space_edit_capacity(request, space_id):
+
+    if request.user.is_staff and request.method == "POST":
+        s = Space.objects.get(id=space_id)
+        try:
+            s.capacity = int(request.POST["capacity"])
+            s.save()
+        except ValueError as ve:
+            print('Value error in space_edit_capacity\n'+ve)
+    return redirect('/space/' + str(space_id))
+
+@login_required
+def space_edit_image(request, space_id):
+
+    if request.user.is_staff and request.method == "POST":
+        u_file = request.FILES["image"]
+        extension = os.path.splitext(u_file.name)[1]
+        s = Space.objects.get(id=space_id)
+        s.image.save(str(space_id)+"_space_image"+extension, u_file)
+        s.save()
+    return redirect('/space/' + str(space_id))
+
+
+@login_required
+def space_edit_description(request, space_id):
+    if request.user.is_staff and request.method == "POST":
+        s = Space.objects.get(id=space_id)
+        s.description = request.POST["description"]
+        s.save()
+    return redirect('/space/' + str(space_id))
