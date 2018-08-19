8fc8336657301be0af6e0be770d81b72d71ac413
Carolina Contreras <ccre.caro@gmail.com>
2018-08-19 13:44:21 -0300

    Espacios en grilla ficha articulo

--- cc4401Inventory/spacesApp/urls.py
+++ cc4401Inventory/spacesApp/urls.py
@@ -9,5 +9,4 @@ urlpatterns = [
     path('<int:space_id>/edit_capacity', views.space_edit_capacity, name='space_edit_capacity'),
     path('<int:space_id>/edit_image', views.space_edit_image, name='space_edit_image'),
     path('<int:space_id>/edit_description', views.space_edit_description, name='space_edit_description'),
-    path('<int:space_id>', views.landing, name='landing'),
 ]
--- cc4401Inventory/spacesApp/views.py
+++ cc4401Inventory/spacesApp/views.py
@@ -34,12 +34,77 @@ def space_delete(request, space_id):
 
 
 @login_required
-def space_data(request, space_id):
+def space_data(request, space_id, date=None):
     try:
         space = Space.objects.get(id=space_id)
+        spaces = Space.objects.all().order_by(Lower('name'))
+
+        if date:
+            current_date = date
+            current_week = datetime.datetime.strptime(current_date, "%Y-%m-%d").date().isocalendar()[1]
+        else:
+            try:
+                current_week = datetime.datetime.strptime(request.GET["date"], "%Y-%m-%d").date().isocalendar()[1]
+                current_date = request.GET["date"]
+            except:
+                current_week = datetime.date.today().isocalendar()[1]
+                current_date = datetime.date.today().strftime("%Y-%m-%d")
+
+        reservations = Reservation.objects.filter(starting_date_time__week=current_week, state__in=['P', 'A'],
+                                                  space__name=space.name)
+
+        colores = {'A': 'rgba(0,153,0,0.7)',
+                   'P': 'rgba(51,51,204,0.7)'}
+
+        res_list = []
+        for i in range(5):
+            res_list.append(list())
+        for r in reservations:
+            reserv = []
+            reserv.append(r.space.name)
+            reserv.append(localtime(r.starting_date_time).strftime("%H:%M"))
+            reserv.append(localtime(r.ending_date_time).strftime("%H:%M"))
+            reserv.append(colores[r.state])
+            res_list[r.starting_date_time.isocalendar()[2] - 1].append(reserv)
+
+        move_controls = list()
+        move_controls.append(
+            (datetime.datetime.strptime(current_date, "%Y-%m-%d") + datetime.timedelta(weeks=-4)).strftime("%Y-%m-%d"))
+        move_controls.append(
+            (datetime.datetime.strptime(current_date, "%Y-%m-%d") + datetime.timedelta(weeks=-1)).strftime("%Y-%m-%d"))
+        move_controls.append(
+            (datetime.datetime.strptime(current_date, "%Y-%m-%d") + datetime.timedelta(weeks=1)).strftime("%Y-%m-%d"))
+        move_controls.append(
+            (datetime.datetime.strptime(current_date, "%Y-%m-%d") + datetime.timedelta(weeks=4)).strftime("%Y-%m-%d"))
+
+        delta = (datetime.datetime.strptime(current_date, "%Y-%m-%d").isocalendar()[2]) - 1
+        monday = (
+            (datetime.datetime.strptime(current_date, "%Y-%m-%d") - datetime.timedelta(days=delta)).strftime(
+                "%d/%m/%Y"))
+        tuesday = ((datetime.datetime.strptime(current_date, "%Y-%m-%d") - datetime.timedelta(days=delta - 1)).strftime(
+            "%d/%m/%Y"))
+        wednesday = (
+            (datetime.datetime.strptime(current_date, "%Y-%m-%d") - datetime.timedelta(days=delta - 2)).strftime(
+                "%d/%m/%Y"))
+        thursday = (
+            (datetime.datetime.strptime(current_date, "%Y-%m-%d") - datetime.timedelta(days=delta - 3)).strftime(
+                "%d/%m/%Y"))
+        friday = ((datetime.datetime.strptime(current_date, "%Y-%m-%d") - datetime.timedelta(days=delta - 4)).strftime(
+            "%d/%m/%Y"))
+
         context = {
             'space': space,
-            'user_is_staff': request.user.is_staff
+            'user_is_staff': request.user.is_staff,
+            'reservations': res_list,
+            'current_date': current_date,
+            'controls': move_controls,
+            'actual_monday': monday,
+            'actual_tuesday': tuesday,
+            'actual_wednesday': wednesday,
+            'actual_thursday': thursday,
+            'actual_friday': friday,
+            'espacios_filtrados': [space.name],
+            'espacios': spaces
         }
         return render(request, 'spacesApp/space_data.html', context)
     except Exception as ex:
@@ -88,69 +153,3 @@ def space_edit_description(request, space_id):
         s.save()
     return redirect('/space/' + str(space_id))
 
-
-def landing(request, date=None, space_id=1):
-    print('hola')
-    spaces = Space.objects.all().order_by(Lower('name'))
-    s = Space.objects.get(id=space_id)
-
-    if date:
-        current_date = date
-        current_week = datetime.datetime.strptime(current_date, "%Y-%m-%d").date().isocalendar()[1]
-    else:
-        try:
-            current_week = datetime.datetime.strptime(request.GET["date"], "%Y-%m-%d").date().isocalendar()[1]
-            current_date = request.GET["date"]
-        except:
-            current_week = datetime.date.today().isocalendar()[1]
-            current_date = datetime.date.today().strftime("%Y-%m-%d")
-
-    reservations = Reservation.objects.filter(starting_date_time__week=current_week, state__in=['P', 'A'],
-                                                  space__name__in=s.name)
-
-    colores = {'A': 'rgba(0,153,0,0.7)',
-               'P': 'rgba(51,51,204,0.7)'}
-
-    res_list = []
-    for i in range(5):
-        res_list.append(list())
-    for r in reservations:
-        reserv = []
-        reserv.append(r.space.name)
-        reserv.append(localtime(r.starting_date_time).strftime("%H:%M"))
-        reserv.append(localtime(r.ending_date_time).strftime("%H:%M"))
-        reserv.append(colores[r.state])
-        res_list[r.starting_date_time.isocalendar()[2] - 1].append(reserv)
-
-    move_controls = list()
-    move_controls.append(
-        (datetime.datetime.strptime(current_date, "%Y-%m-%d") + datetime.timedelta(weeks=-4)).strftime("%Y-%m-%d"))
-    move_controls.append(
-        (datetime.datetime.strptime(current_date, "%Y-%m-%d") + datetime.timedelta(weeks=-1)).strftime("%Y-%m-%d"))
-    move_controls.append(
-        (datetime.datetime.strptime(current_date, "%Y-%m-%d") + datetime.timedelta(weeks=1)).strftime("%Y-%m-%d"))
-    move_controls.append(
-        (datetime.datetime.strptime(current_date, "%Y-%m-%d") + datetime.timedelta(weeks=4)).strftime("%Y-%m-%d"))
-
-    delta = (datetime.datetime.strptime(current_date, "%Y-%m-%d").isocalendar()[2]) - 1
-    monday = (
-        (datetime.datetime.strptime(current_date, "%Y-%m-%d") - datetime.timedelta(days=delta)).strftime("%d/%m/%Y"))
-    tuesday = ((datetime.datetime.strptime(current_date, "%Y-%m-%d") - datetime.timedelta(days=delta - 1)).strftime(
-        "%d/%m/%Y"))
-    wednesday = ((datetime.datetime.strptime(current_date, "%Y-%m-%d") - datetime.timedelta(days=delta - 2)).strftime(
-        "%d/%m/%Y"))
-    thursday = ((datetime.datetime.strptime(current_date, "%Y-%m-%d") - datetime.timedelta(days=delta - 3)).strftime(
-        "%d/%m/%Y"))
-    friday = ((datetime.datetime.strptime(current_date, "%Y-%m-%d") - datetime.timedelta(days=delta - 4)).strftime(
-        "%d/%m/%Y"))
-    context = {'reservations': res_list,
-               'current_date': current_date,
-               'controls': move_controls,
-               'actual_monday': monday,
-               'actual_tuesday': tuesday,
-               'actual_wednesday': wednesday,
-               'actual_thursday': thursday,
-               'actual_friday': friday,
-               'space_id': s,
-               'espacios': spaces}
-    return render(request, 'space_data.html', context)
