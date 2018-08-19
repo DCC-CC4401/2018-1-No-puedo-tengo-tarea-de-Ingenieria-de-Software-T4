d7fbf9be042b991e526fd708e85ab0eb49239c79
Mortup <g.uribemontero@gmail.com>
2018-08-19 16:50:32 +0000

    Se puede reservar desde la ficha de espacios

--- cc4401Inventory/spacesApp/urls.py
+++ cc4401Inventory/spacesApp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from . import views
+import mainApp
 
 urlpatterns = [
     path('<int:space_id>/delete', views.space_delete, name='space_delete'),
@@ -9,4 +10,6 @@ urlpatterns = [
     path('<int:space_id>/edit_capacity', views.space_edit_capacity, name='space_edit_capacity'),
     path('<int:space_id>/edit_image', views.space_edit_image, name='space_edit_image'),
     path('<int:space_id>/edit_description', views.space_edit_description, name='space_edit_description'),
+    path('newReservation', mainApp.views.new_reservation, name='msg_nueva_reserva'),
+    path('makeReservation', mainApp.views.make_reservation, name='do_reserva'),
 ]
